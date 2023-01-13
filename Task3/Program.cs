5d8549d31ae3a5bd15fdd1d6fb7968cf957d9fb3
Grigoriy Kapranov <kapranovgu@mail.ru>
2023-01-13 22:20:44 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+System.Console.WriteLine("Введите число >");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Число {number} - чётное");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} - нечётное");
+}
