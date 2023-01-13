b01e8fe3e27390429e96c55b3935ec61310ab3e6
Grigoriy Kapranov <kapranovgu@mail.ru>
2023-01-13 22:42:58 +0300

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 4: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+System.Console.WriteLine("Введите число >");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+if (number <= 1)
+{
+    System.Console.WriteLine("Нет чётных чисел для заданного условия");
+}
+
+{
+    while (count <= number)
+    {
+        if (count % 2 == 0 && count > 0)
+        {
+            System.Console.Write($"{count} ");
+        }
+        count++;
+    }
+}
